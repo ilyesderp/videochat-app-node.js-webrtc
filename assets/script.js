3e89eec358253a18d22dd3ce899e46a8800e3cdb
Ubuntu <ubuntu@vps-b1439c92.vps.ovh.net>
2021-09-30 14:25:39 +0000

    basic app working even through NAT with heroku peerjs server

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,124 @@
-const socket = io('/', {transports: ['websocket']})
+const socket = io();
 const videoGrid = document.getElementById('videoGrid')
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
 var peer = new Peer(undefined, {
-    host: '/',
-    //port: '9000',
-    path: '/peerjs',
-    secure: true,
-    config: {'iceServers': [
-		{ url: 'stun:stun.l.google.com:19302' },
-		{ url: 'stun:stun1.l.google.com:19302' },
-		{ url: 'stun:stun2.l.google.com:19302' },
-		{ url: 'stun:stun3.l.google.com:19302' },
-		{ url: 'stun:stun4.l.google.com:19302' },
-		{ url: 'stun:stun01.sipphone.com' },
-		{ url: 'stun:stun.ekiga.net' },
-		{ url: 'stun:stun.fwdnet.net' },
-		{ url: 'stun:stun.ideasip.com' },
-		{ url: 'stun:stun.iptel.org' },
-		{ url: 'stun:stun.rixtelecom.se' },
-		{ url: 'stun:stun.schlund.de' },
-    	{ url: 'stun:stunserver.org' },
-		{ url: 'stun:stun.softjoys.com' },
-		{ url: 'stun:stun.voiparound.com' },
-		{ url: 'stun:stun.voipbuster.com' },
-		{ url: 'stun:stun.voipstunt.com' },
-		{ url: 'stun:stun.voxgratia.org' },
-		{ url: 'stun:stun.xten.com' },
-        { url: 'turn:turn.newton-schools.com', credential: 'NUM509022', username: 'ilyes' }
-      ]} 
-  })
+	//path: '/myapp',
+	//host: 'newton-schools.com',
+	//port: 4000,
+	host:'peerjs-server.herokuapp.com', 
+	port:443,
+	secure: true,
+	iceServers: [{
+		urls: 'turn:numb.viagenie.ca',
+		credential: 'muazkh',
+		username: 'webrtc@live.com'
+	},
+	{
+		urls: 'turn:192.158.29.39:3478?transport=udp',
+		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+		username: '28224511:1379330808'
+	},
+	{
+		urls: 'turn:192.158.29.39:3478?transport=tcp',
+		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+		username: '28224511:1379330808'
+	},
+	{
+		urls: 'turn:turn.bistri.com:80',
+		credential: 'homeo',
+		username: 'homeo'
+	 },
+	 {
+		urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+		credential: 'webrtc',
+		username: 'webrtc'
+	}, 
+	{ urls: 'turn:turn.newton-schools.com', credential: 'NUM509022', username: 'ilyes' },
+	{
+		urls: [
+			  'stun:stun.l.google.com:19302' ,
+			  'stun:stun1.l.google.com:19302' ,
+			  'stun:stun2.l.google.com:19302' ,
+			  'stun:stun3.l.google.com:19302' ,
+			  'stun:stun4.l.google.com:19302' ,
+			  'stun:stun01.sipphone.com' ,
+			  'stun:stun.ekiga.net' ,
+			  'stun:stun.fwdnet.net' ,
+			  'stun:stun.ideasip.com' ,
+			  'stun:stun.iptel.org' ,
+			  'stun:stun.rixtelecom.se' ,
+			  'stun:stun.schlund.de' ,
+			  'stun:stunserver.org' ,
+			  'stun:stun.softjoys.com' ,
+			  'stun:stun.voiparound.com' ,
+			  'stun:stun.voipbuster.com' ,
+			  'stun:stun.voipstunt.com' ,
+			  'stun:stun.voxgratia.org' ,
+			  'stun:stun.xten.com' ,
+		]
+	}]
+})
+
 
 const myPeer = new Peer(undefined, {
-    host: '/',
-    //port: '9000',
-    path: '/peerjs',
-    secure: true,
-    config: {'iceServers': [
-		{ url: 'stun:stun.l.google.com:19302' },
-		{ url: 'stun:stun1.l.google.com:19302' },
-		{ url: 'stun:stun2.l.google.com:19302' },
-		{ url: 'stun:stun3.l.google.com:19302' },
-		{ url: 'stun:stun4.l.google.com:19302' },
-		{ url: 'stun:stun01.sipphone.com' },
-		{ url: 'stun:stun.ekiga.net' },
-		{ url: 'stun:stun.fwdnet.net' },
-		{ url: 'stun:stun.ideasip.com' },
-		{ url: 'stun:stun.iptel.org' },
-		{ url: 'stun:stun.rixtelecom.se' },
-		{ url: 'stun:stun.schlund.de' },
-    	{ url: 'stun:stunserver.org' },
-		{ url: 'stun:stun.softjoys.com' },
-		{ url: 'stun:stun.voiparound.com' },
-		{ url: 'stun:stun.voipbuster.com' },
-		{ url: 'stun:stun.voipstunt.com' },
-		{ url: 'stun:stun.voxgratia.org' },
-		{ url: 'stun:stun.xten.com' },
-        { url: 'turn:turn.newton-schools.com', credential: 'NUM509022', username: 'ilyes' }
-      ]} 
-  });
+	//path: '/myapp',
+	//host: 'newton-schools.com',
+	//port: 4000,
+	host:'peerjs-server.herokuapp.com', 
+	port:443,
+	secure: true,
+	iceServers: [{
+		urls: 'turn:numb.viagenie.ca',
+		credential: 'muazkh',
+		username: 'webrtc@live.com'
+	},
+	{
+		urls: 'turn:192.158.29.39:3478?transport=udp',
+		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+		username: '28224511:1379330808'
+	},
+	{
+		urls: 'turn:192.158.29.39:3478?transport=tcp',
+		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+		username: '28224511:1379330808'
+	},
+	{
+		urls: 'turn:turn.bistri.com:80',
+		credential: 'homeo',
+		username: 'homeo'
+	 },
+	 {
+		urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+		credential: 'webrtc',
+		username: 'webrtc'
+	}, 
+	{ urls: 'turn:turn.newton-schools.com', credential: 'NUM509022', username: 'ilyes' },
+	{
+		urls: [
+			  'stun:stun.l.google.com:19302' ,
+			  'stun:stun1.l.google.com:19302' ,
+			  'stun:stun2.l.google.com:19302' ,
+			  'stun:stun3.l.google.com:19302' ,
+			  'stun:stun4.l.google.com:19302' ,
+			  'stun:stun01.sipphone.com' ,
+			  'stun:stun.ekiga.net' ,
+			  'stun:stun.fwdnet.net' ,
+			  'stun:stun.ideasip.com' ,
+			  'stun:stun.iptel.org' ,
+			  'stun:stun.rixtelecom.se' ,
+			  'stun:stun.schlund.de' ,
+			  'stun:stunserver.org' ,
+			  'stun:stun.softjoys.com' ,
+			  'stun:stun.voiparound.com' ,
+			  'stun:stun.voipbuster.com' ,
+			  'stun:stun.voipstunt.com' ,
+			  'stun:stun.voxgratia.org' ,
+			  'stun:stun.xten.com' ,
+		]
+	}]
+})
 
 
   const peers = {}
@@ -74,7 +133,8 @@ navigator.mediaDevices
 		addVideoStream(myVideo, stream)
 
 		socket.on('user-connected', (userId) => {
-			setTimeout(connectToNewUser(userId, stream), 3000)
+			setTimeout(connectToNewUser, 3000, userId, stream)
+			//connectToNewUser(userId, stream);
 			alert('Somebody connected', userId)
 		})
 
@@ -115,7 +175,7 @@ navigator.mediaDevices
 							</li>`)
 			scrollToBottom()
 		})
-	})
+	}).catch(function(err) { console.log(err.name + ": " + err.message); });
 
 socket.on('user-disconnected', (userId) => {
 	if (peers[userId]) peers[userId].close()
@@ -139,6 +199,8 @@ const connectToNewUser = (userId, stream) => {
 }
 
 const addVideoStream = (video, stream) => {
+	console.log(video)
+	console.log(stream)
 	video.srcObject = stream
 	video.addEventListener('loadedmetadata', () => {
 		video.play()
--- server.js
+++ server.js
@@ -1,77 +1,51 @@
-//var fs = require('fs');
+var fs = require('fs');
 const express = require('express')
 const app = express()
-const http = require('http')
-//const https = require('https');
-
-var httpServer = http.createServer(app);
-
-const { Server } = require("socket.io");
-const io = new Server(httpServer, { allowEIO3: true });
-
-const { v4: uuidV4 } = require('uuid')
-const { ExpressPeerServer } = require('peer');
-
-
-// setting peerjs server:
-//var PeerServer = require('peer').PeerServer;
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    //proxied: true,
+    debug: true,
+    path: '/myapp',
+    ssl: { key: fs.readFileSync('/etc/letsencrypt/live/document-sharing-app.newton-schools.com/privkey.pem'),
+    	   cert: fs.readFileSync('/etc/letsencrypt/live/document-sharing-app.newton-schools.com/fullchain.pem') 
+		}
+})
+const { v4: uuidv4 } = require('uuid')
 
 
-/*var server2 = PeerServer({
-  port: 9000,
-  path: '/myapp',
-  ssl: {
-    key: fs.readFileSync('/etc/letsencrypt/live/newton-schools.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/newton-schools.com/fullchain.pem')
-  },
-  proxied: true
-});*/
-
-
-
-const PORT = process.env.PORT || 4000
-
-httpServer.listen(PORT)
-
-
-const peerServer = ExpressPeerServer(httpServer, {
-  debug: true,
-});
-
-app.use('/peerjs', peerServer);
-
-
-
-app.set('view engine', 'ejs')
+app.use('/peerjs', peerServer)
 app.use(express.static('assets'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+	res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+	res.render('room', { roomId: req.params.room })
 })
-
-
-
-
 
 io.on('connection', (socket) => {
 	socket.on('join-room', (roomId, userId) => {
 		socket.join(roomId)
-		//socket.to(roomId).broadcast.emit('user-connected', userId) // error: undefined.
-    socket.broadcast.to(roomId).emit('user-connected', userId) //works
+		socket.broadcast.to(roomId).emit('user-connected', userId) //works
 
 		socket.on('message', (message) => {
 			io.to(roomId).emit('createMessage', message, userId)
 		})
 		socket.on('disconnect', () => {
-			//socket.to(roomId).broadcast.emit('user-disconnected', userId)//error: undefined
       socket.broadcast.to(roomId).emit('user-disconnected', userId)
 		})
 	})
 })
 
+const PORT = process.env.PORT || 4000
+const HOSTNAME = "newton-schools.com";
+//server.listen(PORT)
 
 
+server.listen(PORT, HOSTNAME, () => {
+	console.log(`Server running at https://${HOSTNAME}:${PORT}/`);
+  });
